64f76bb3e227b2592b54959b6a4d89ee82b9ff64
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-20 22:09:47 -0500

    Added basic express server structure

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import path from "path";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import express from "express";
+import { fileURLToPath } from "url";
+import body_parser from "body-parser";
+import compression from "compression";
+
+
+// Initalization
+dotenv.config();
+const app = express();
+
+
+// Configuration
+global.__dirname = path.dirname(fileURLToPath(import.meta.url));
+global.debug = (msg) => {
+  process.env.DEBUG && console.log(msg);
+}
+
+
+// Middleware
+app.use(helmet());
+app.use(compression());
+app.use(body_parser.json());
+
+
+// Execution
+app.listen(process.env.PORT || 2011, () => {
+  console.log(`Server Started on ${process.env.PORT || "2011"}`)
+})
