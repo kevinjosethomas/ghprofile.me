f1740b18176413484ad5d5cff00f436e63b0fdda
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 10:20:59 -0500

    Added asynchronous functions

--- src/api/view.js
+++ src/api/view.js
@@ -2,8 +2,31 @@ import express from "express";
 
 const router = express.Router();
 
+
+async function get_view_count(username) {
+
+  let response = await client.query(
+    "SELECT  * FROM profile_views WHERE username = $1",
+    [username]
+  )
+
+  return response;
+
+};
+
+async function increment_view_count(username) {
+
+  const timestamp = Math.floor(Date.now() / 1000)
+
+  await client.query(
+    "INSERT INTO profile_views (username, timestamp) VALUES ($1, $2)",
+    [username.slice(0, 40), timestamp]
+  )
+
+}
+
 router.get("/", async (req, res) => {
 
-})
+});
 
-export default router
+export default router;
