376b079c4577aa26e8f0abfc98375622ff555bda
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 13:27:58 -0500

    Removed debugging

--- src/api/view.js
+++ src/api/view.js
@@ -7,9 +7,6 @@ router.get("/", async (req, res) => {
 
   const username = req.query.username;
 
-  console.log(req.headers)
-  console.log("\n\n")
-
   if (!username) {
     res.status(400).json({
       success: false,
