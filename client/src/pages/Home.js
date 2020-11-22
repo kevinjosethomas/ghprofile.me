c34680724c8ea628ffb93f697ebccfc6b456f077
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 21:04:33 -0500

    Added Navbar to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
 // Modules
 import React from "react";
 
+// Components
+import Navbar from "./components/Navbar.js";
+
 
 export default class Home extends React.Component {
 
   render() {
     return (
       <div className="bg-gray-300 w-screen h-screen">
-
+        <Navbar />
       </div>
     )
   }
