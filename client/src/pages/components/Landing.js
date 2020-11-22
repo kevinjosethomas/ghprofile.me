69ba29cb0b2259e95c671fe27d3dd6670ecbffef
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 21:58:24 -0500

    Added Landing component

---
+++ client/src/pages/components/Landing.js
@@ -0,0 +1,14 @@
+
+export default function Landing() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="flex flex-col items-center justify-center py-48 px-2 text-center">
+        <span className="text-5xl md:text-6xl font-inter font-bold leading-none">Automated <span className="text-indigo-600">Statistics</span></span>
+        <span className="mt-2 md:max-w-2xl text-lg md:text-xl font-inter font-light leading-tight">Free GitHub analytics and statistics for personal and professional services. Discover new growth possibilities by efficiently analyzing statistics. </span>
+      </div>
+
+  </div>
+  )
+
+}
