93ff036dfc96de82100f710425b6eb2b2766fc00
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 21:59:36 -0500

    Added request functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,37 @@
 // Modules
+import axios from "axios";
 import React from "react";
 
 // Components
 import Navbar from "./components/Navbar.js";
+import Landing from "./components/Landing.js";
 
 
 export default class Home extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  async componentDidMount() {
+
+    let response = await axios.get("/historic/view?username=TrustedMercury")
+    this.setState({
+      data: response.data
+    });
+
+  }
+
   render() {
+    let data = "hello"
+
     return (
       <div className="bg-gray-300 w-screen h-screen">
         <Navbar />
+        <Landing data={this.state.data} />
       </div>
     )
   }
