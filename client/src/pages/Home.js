e894c544b5aa046265488d08bb65f4e2806fc44f
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 20:16:41 -0500

    Minor Structural Changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
+// Modules
 import React from "react";
+
 
 export default class Home extends React.Component {
 
   render() {
     return (
       <div className="bg-gray-300 w-screen h-screen">
-        
+
       </div>
     )
   }
