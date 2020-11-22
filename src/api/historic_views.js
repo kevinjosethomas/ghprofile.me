7ce23185b81520c22474e6ec7e2fdee6fe9777e0
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-22 11:39:37 -0500

    Added periods to historic API response

--- src/api/historic_views.js
+++ src/api/historic_views.js
@@ -21,14 +21,24 @@ router.get("/", async (req, res) => {
     }).end();
   }
 
-  let response = await client.query(
+  // Fetches views per day, over time
+  let graph = await client.query(
     "select s1.reporting_date, case when s2.no_of_views is null then 0 else s2.no_of_views end as no_of_views from (SELECT generate_series(now() - interval '29' day, now(), '1 day')::date as reporting_date) as s1 left join (select date(timestamp) reporting_date_1, count(*) no_of_views from profile_views where username = $1 group by date(timestamp)) as s2 on s1.reporting_date = s2.reporting_date_1",
+    [username.toLowerCase()]
+  )
+
+  // Fetches views per period
+  let period = await client.query(
+    "select sum(is_user) as _all_time, sum(in_30days) as _month, sum(in_14days) as _fortnight, sum(in_7days) as _week, sum(in_day) as _day, sum(in_hour) as _hour from (select 1 as is_user,case when timestamp > (now() - interval '30' day) then 1 else 0 end as in_30days,case when timestamp > (now() - interval '14' day) then 1 else 0 end as in_14days, case when timestamp > (now() - interval '7' day) then 1 else 0 end as in_7days, case when timestamp > (now() - interval '1' day) then 1 else 0 end as in_day, case when timestamp > (now() - interval '1' hour) then 1 else 0 end as in_hour from profile_views where username = $1) as sq1",
     [username.toLowerCase()]
   )
 
   return res.status(200).json({
     success: true,
-    payload: response.rows
+    payload: {
+      graph: graph.rows,
+      period: period.rows[0]
+    }
   }).end();
 
 })
