6e5320b64c6d2cabb272a3ef6b251e784d742bf4
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 20:15:17 -0500

    Added routing and tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Line } from 'react-chartjs-2';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Home from "./pages/Home.js";
 
+import Tailwind from "./styles/tailwind.css";
+
 function App() {
   return (
-    <React.Fragment>
-      <Home />
-    </React.Fragment>
+    <Router>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
