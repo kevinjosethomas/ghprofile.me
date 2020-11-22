32be4ebb5c5febe7f3237faea706a696b9815346
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-21 21:04:06 -0500

    Added Navbar component

---
+++ client/src/pages/components/Navbar.js
@@ -0,0 +1,16 @@
+// Modules
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return(
+    <div className="flex flex-row items-center justify-between w-screen py-3 px-5 md:px-20 bg-gray-400">
+      <div class="flex flex-row items-center justify-center">
+        <Link to="/" class="text-2xl md:text-3xl text-gray-800 font-semibold font-inter">ghprofile.me</Link>
+      </div>
+      <div class="flex flex-row items-center justify-center">
+        <a href="https://docs.ghprofile.me" target="_blank" class="fas fa-book mr-2 text-3xl md:text-4xl text-gray-800"></a>
+        <a href="https://github.com/TrustedMercury/ghprofile.me" target="_blank" class="fab fa-github ml-2 text-3xl md:text-4xl text-gray-800"></a>
+      </div>
+    </div>
+  )
+}
