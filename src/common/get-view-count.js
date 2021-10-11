8419b46fccdb1f8de4a9668667db33623cfbb6a6
Alexis <xo@wvvw.me>
2021-10-11 16:41:01 +1030

    fix: ensure views cache is always updated on init

--- src/common/get-view-count.js
+++ src/common/get-view-count.js
@@ -11,5 +11,13 @@ export const getViewCount = async (name) => {
   const pool = await getPool();
   const query = SQL`SELECT COUNT(*) FROM views WHERE name = ${name}`;
   const count = await pool.query(query);
+
+  // Create initial cache
+  views[name] = {
+    cachedViews: [],
+    totalViewCount: count
+  };
+
+  // Return views
   return count.rowCount ? count.rows[0].count : 0;
 };
