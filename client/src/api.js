2a2174e145c39e6a14918bac11ac64e73ddb1f82
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-26 00:08:45 -0800

    feat: cleaned up old version

--- client/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? process.env.REACT_APP_TEST_API_URL : process.env.REACT_APP_API_URL
-});
-
--- setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS views (
-  name VARCHAR,
-  timestamp TIMESTAMPTZ
-);
