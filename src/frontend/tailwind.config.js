e8e34240d0a4572c3f2a07652c55b6a3780c9b74
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-26 22:31:10 -0800

    feat: added tailwindcss template

---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.jsx", "./components/**/*.jsx"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
